0999d064361ee4bdd21f25fd3af6e00a255e0b27
ihseo <inhyukseo1@gmail.com>
2019-11-23 23:37:07 +0900

    add db connection information

---
+++ dbcon.php
@@ -0,0 +1,12 @@
+<?php
+
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    'mariadb',
+    'shinhan');
+$sql = "SELECT * FROM techtrade";
+$result = mysqli_query($conn, $sql);
+$conn->set_charset("utf8mb4");
+
+?>
